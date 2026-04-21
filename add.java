2942f656f03613d3cc9a58890512fd01260c3bef
gauravyadav73670-dot <gauravyadav73670@gmail.com>
2026-04-21 11:03:15 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,10 @@
+class add {
+    public static void main(string[]args)
+    {
+        int num1, num2, sum;
+        num1=2;
+        num2=5;
+        sum=num1+num2;
+        system.out.println("result is"+sum);
+    }
+}
